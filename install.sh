25b46585e42f5d87c1865566ad3850d107eb7714
Mike Turkey <sign@noreply.miketurkey.com>
2024-11-25 18:57:34 +0900

    delete *.dusg files

--- install.sh
+++ install.sh
@@ -3,14 +3,14 @@
 T=$(dirname "$0")
 SCRDIR=$(cd "$T"; pwd)
 CONF="$SCRDIR"/snakeland-mk1pass.conf
-INSTALLSNAKE="$SCRDIR"/tools/install-snakeland.sh
+INSTALLSNAKE="$SCRDIR"/snakeland/install.sh
 
 ### Function ###
 check_rootuser(){
     local ARGSUSER TMP_USER
     ARGSUSER='root'
     TMP_USER=$(id -p | grep uid | awk '{print $2}')
-    if test "$ARGSUSER" != "$TMP_USER"; then
+    if [ "$ARGSUSER" != "$TMP_USER" ]; then
         echo 'Error: Not root user.' " [uid: $TMP_USER]"
         exit 1
     fi
@@ -30,4 +30,4 @@ if ! snakeland --version > /dev/null 2>&1; then
 fi
 
 snakeland install "$CONF"  || exit 1
-
+exit 0
